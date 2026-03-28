0de402127ced4300f2c80c4f12fd3c034c5fe900
byehi5321 <byehi5321@gmail.com>
2026-03-28 14:14:21 +0530

    chore(scraper): remove moviesmod scraper module
    The `moviesmod` scraper module and all its associated loading and configuration
    references have been removed from the project.

--- check_scrapers.js
+++ check_scrapers.js
@@ -10,7 +10,7 @@ const files = [
   // "./src/scrapers/mallumv",
   // "./src/scrapers/mapple",
   // "./src/scrapers/moviebox",
-  "./src/scrapers/moviesmod",
+  // "./src/scrapers/moviesmod",
   "./src/scrapers/myflixer-extractor",
   "./src/scrapers/netmirror",
   "./src/scrapers/showbox",
